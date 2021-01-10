83a380a9f6294f0e27f7a2b47141d4afeded9ab8
Marcos Silvano <marcossilvano@gmail.com>
2021-01-09 21:12:22 -0300

    Polimorfismo sobrecarga metodo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Polimorfismo_Sobrecarga_Metodo_POO.classes;
+
+namespace Polimorfismo_Sobrecarga_Metodo_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instaciamento da classe 
+            Funcionario fun = new Funcionario();
+            fun.Mostrar();
+            fun.Mostrar(2);
+            fun.Mostrar("Tsuka");
+
+            Console.Beep();
+        }
+    }
+}
